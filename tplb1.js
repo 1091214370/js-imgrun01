65de80c8fdfa044ac73ff7a6fbc31e2e5201a21f
xch <1091214370@qq.com>
2017-02-25 22:17:25 +0800

    优化了鼠标点击导致自动轮播延时错乱的BUG

--- tplb1.js
+++ tplb1.js
@@ -6,33 +6,8 @@ window.onload=function (){
 	var olaf=document.getElementById("ol-f");
 	var olas=document.getElementById("ol-s");
 	var olat=document.getElementById("ol-t");
-	var timeId;
+	var timeId=null;
 	var i=0;
-	tl.onclick=function(){
-		i+=1;
-		i=i%3;
-		listTime();
-		if(i>0&&i<3){
-			var si =100 * i;
-			road.style.marginLeft="-"+si+"%";
-		}else{
-			road.style.marginLeft=0;
-			i=0;
-		}
-		}
-	tr.onclick=function(){
-		i+=2;
-		i=i%3;
-		listTime();
-		if(i>0&&i<3){
-			var si =(100 * i);
-			road.style.marginLeft="-"+si+"%";
-		}else{
-			road.style.marginLeft="0";
-			i=0;
-		}
-	}
-		timeId=window.setInterval(tl.onclick,3000);
 	function listTime(){
 			switch(i){
 				case 0:
@@ -53,6 +28,46 @@ window.onload=function (){
 	
 			}
 	}	
-		timeId();
+	function tpturnLeft(){
+		i+=2;
+		i=i%3;
+		listTime();
+		if(i>0&&i<3){
+			var si =(100 * i);
+			road.style.marginLeft="-"+si+"%";
+		}else{
+			road.style.marginLeft="0";
+			i=0;
+		}
+	}
+	function tpturnRight(){
+		i+=1;
+		i=i%3;
+		listTime();
+		if(i>0&&i<3){
+			var si =100 * i;
+			road.style.marginLeft="-"+si+"%";
+		}else{
+			road.style.marginLeft=0;
+			i=0;
+		}
+	}
+	timeId=setInterval(tpturnRight,3000);
+	tl.onclick=function(){
+		if(timeId){
+			clearInterval(timeId);
+			timeId=null;
+		}
+		tpturnRight();
+		timeId=setInterval(tpturnRight,3000);
+	}
+	tr.onclick=function(){
+		if(timeId){
+			clearInterval(timeId);	
+			timeId=null;
+		}
+		tpturnLeft();
+		timeId=setInterval(tpturnRight,3000);
+	}
+
 }
-	
